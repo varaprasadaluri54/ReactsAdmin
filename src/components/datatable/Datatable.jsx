ce6579b35977ca3031719c2df2a146cd745012ab
Aluri varaprasad <38999815+varaprasadaluri54@users.noreply.github.com>
2022-04-07 08:11:43 -0400

    src

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -3,13 +3,28 @@ import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
 
 export default function Datatable() {
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: () => {
+        return (
+          <div className="cellAction">
+            <div className="viewButton">View</div>
+            <div className="deleteButton">Delete</div>
+          </div>
+        );
+      }
+    }
+  ];
   return (
     <div className="datatable">
       <DataGrid
         rows={userRows}
-        columns={userColumns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        columns={userColumns.concat(actionColumn)}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
         checkboxSelection
       />
     </div>
--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,5 +1,30 @@
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./single.scss";
-
+import Navbar from "../../components/navbar/Navbar";
 export default function Single() {
-  return <div className="single">Single</div>;
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?cs=srgb&dl=pexels-mohamed-abdelghaffar-771742.jpg&fm=jpg"
+                alt=""
+                className="itemImg"
+              />
+            </div>
+          </div>
+          <div className="right">
+            <h1 className="title">right</h1>
+          </div>
+        </div>
+        <div className="buttom"></div>
+        single
+      </div>
+    </div>
+  );
 }
